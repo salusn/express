b4e9e42f4b7a188fe1649d533922f6e4df877e6d
salusn <salu.s@lightrains.com>
2017-11-10 11:54:50 +0530

    Fetched data from table for a particular user

--- routes/edit.js
+++ routes/edit.js
@@ -1,32 +1,21 @@
 var express = require('express');
 var router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
+var ObjectID = require('mongodb').ObjectID;
 assert = require('assert');
 var url = 'mongodb://localhost:27017/formdb';
 
-
 router.get('/edit/:id', function(req, res, next) {
 
-// MongoClient.connect(url, function(err, db) {	
-//   console.log(req.params.id)
-//    var id = req.params.id;
-//   formdata.find({:id}, function(err, docs){
-// // collection.find(condition ,function(err, companies) {
-// //     companies.each(function(err, company){
-// //         //do smth with found
-// //     });
+	MongoClient.connect(url, function(err, db) {	
+		var id = req.params.id;
+		var collection = db.collection('formdata');
+		var o_id = new ObjectID(id);
 
-//   //	db.collection('formdata').find(req.params.id).toArray(function(err, docs){
-// 			if(err) res.json(err);
-// 			else    
-// console.log(docs)
-
-// 				res.render('edit', {_id: req.params.id});
-// 		});	
-
-//   //res.render('edit', {_id: req.params.id});
-// })
-res.render('edit', {id: req.params.id});
+		collection.find({_id:o_id}).toArray(function(err, docs){
+        	res.render('edit', {data: docs});
+   		});		
+	});
 });
 
 module.exports = router;
